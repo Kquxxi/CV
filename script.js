0bf9d4e59a5c4624e0ca4e952772e12c6a9f8ef1
Konrad <kquxxi@gmail.com>
2024-06-21 23:54:31 +0200

    init

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
+function toggleMenu() {
+    var navbar = document.getElementById('navbar');
+    navbar.classList.toggle('show-menu');
+}
 
-        function openPopup(imageSrc, title, description) {
-            document.getElementById('popup-image').src = imageSrc;
+function openPopup(imageSrc, title, description) {
+    document.getElementById('popup-image').src = imageSrc;
+    document.getElementById('popup').style.display = 'flex';
+}
 
-            document.getElementById('popup').style.display = 'flex';
-        }
-        function closePopup() {
-            document.getElementById('popup').style.display = 'none';
-        }
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
+function toggleTheme() {
+    var body = document.body;
+    body.classList.toggle('dark-mode');
+
+    var navbar = document.getElementById('navbar');
+    navbar.classList.toggle('dark-mode');
+
+    var links = document.querySelectorAll('#navbar a');
+    links.forEach(link => {
+        link.classList.toggle('dark-mode');
+    });
+
+    var tiles = document.querySelectorAll('.project-tile, .skill-tile');
+    tiles.forEach(tile => {
+        tile.classList.toggle('dark-mode');
+    });
+
+    var contactButton = document.getElementById('contact-me-btn');
+    contactButton.classList.toggle('dark-mode');
+}
